70c3eca48ec58a324a5b86dc9ffc89ff91a78933
bekhairga <55501696+bekhairga@users.noreply.github.com>
2021-02-20 18:03:28 +0500

    Added faq section template

---
+++ src/components/FaqSection.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>Any questions <span>FAQ</span></h2>
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, sunt!</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Dialy Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, sunt!</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, sunt!</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Products Do You Offer</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, sunt!</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FaqSection
