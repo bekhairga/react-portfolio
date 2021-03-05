ac03f93aa4a0dd1a132178da498748c28e16157e
BekhaIrga <irgashevbehzod@gmail.com>
2021-03-06 00:14:49 +0500

    Added routing and links were setuped

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import styled from 'styled-components';
+import {Link} from 'react-router-dom';
 const Nav = () => {
     return (
         <StyledNav>
             <h1>
-                <a id="logo" href="#">Capture</a>
+                <Link id="logo" to="/">Capture</Link>
             </h1>
             <ul>
                 <li>
-                    <a href="#">1. About us</a>
+                    <Link to="/">1. About us</Link>
                 </li>
                 <li>
-                    <a href="#">2. Our Work</a>
+                    <Link to="/work">2. Our Work</Link>
                 </li>
                 <li>
-                    <a href="#">3. Contact Us</a>
+                    <Link to="/contact">3. Contact Us</Link>
                 </li> 
             </ul>
         </StyledNav>
@@ -43,6 +44,7 @@ const StyledNav = styled.nav`
     }
     li{
         padding-left: 10rem;
+        position: relative;
     }
 `;
 
